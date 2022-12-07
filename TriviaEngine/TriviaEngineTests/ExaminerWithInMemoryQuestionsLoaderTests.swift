64069e7cede43bba653514009b92a6bbdaa86ab1
Paulo Sérgio <ps131296@gmail.com>
2022-12-07 15:46:18 -0300

    `Examiner` and `InMemoryQuestionsLoader` delivers final final score "2" and answers "correct" when user responds all questions right

--- TriviaEngine/TriviaEngineTests/ExaminerWithInMemoryQuestionsLoaderTests.swift
+++ TriviaEngine/TriviaEngineTests/ExaminerWithInMemoryQuestionsLoaderTests.swift
@@ -16,4 +16,19 @@ class ExaminerWithInMemoryQuestionsLoaderTests: XCTestCase {
         XCTAssertEqual(score.responses.first?.isCorrect, false)
         XCTAssertEqual(score.responses.last?.isCorrect, false)
     }
+
+    func test_examinerWithInMemoryQuestionsLoader_deliversFullScoreWhenRespondingWithCorrectAnswers() throws {
+        let inMemoryQuestionsLoader = InMemoryQuestionsLoader()
+        let examiner = Examiner(questionsLoader: inMemoryQuestionsLoader)
+
+        let firstQuestion = try examiner.start()
+        let secondQuestion = examiner.respond(firstQuestion, with: firstQuestion.answers[2])!
+        _ = examiner.respond(secondQuestion, with: secondQuestion.answers[2])
+
+        let score = examiner.evaluate()
+
+        XCTAssertEqual(score.points, 2)
+        XCTAssertEqual(score.responses.first?.isCorrect, true)
+        XCTAssertEqual(score.responses.last?.isCorrect, true)
+    }
 }
