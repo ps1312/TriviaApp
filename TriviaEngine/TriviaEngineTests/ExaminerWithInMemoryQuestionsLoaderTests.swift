5214adfeecc97024807984226cd2551699d6c234
Paulo Sérgio <ps131296@gmail.com>
2022-12-07 15:47:27 -0300

    add score "1" test with only first correct answer to finish the test triangulation

--- TriviaEngine/TriviaEngineTests/ExaminerWithInMemoryQuestionsLoaderTests.swift
+++ TriviaEngine/TriviaEngineTests/ExaminerWithInMemoryQuestionsLoaderTests.swift
@@ -17,6 +17,21 @@ class ExaminerWithInMemoryQuestionsLoaderTests: XCTestCase {
         XCTAssertEqual(score.responses.last?.isCorrect, false)
     }
 
+    func test_examinerWithInMemoryQuestionsLoader_deliversFinalScoreScoreWhenRespondingWithOneRightAnswer() throws {
+        let inMemoryQuestionsLoader = InMemoryQuestionsLoader()
+        let examiner = Examiner(questionsLoader: inMemoryQuestionsLoader)
+
+        let firstQuestion = try examiner.start()
+        let secondQuestion = examiner.respond(firstQuestion, with: firstQuestion.answers[0])!
+        _ = examiner.respond(secondQuestion, with: secondQuestion.answers[2])
+
+        let score = examiner.evaluate()
+
+        XCTAssertEqual(score.points, 1)
+        XCTAssertEqual(score.responses.first?.isCorrect, false)
+        XCTAssertEqual(score.responses.last?.isCorrect, true)
+    }
+
     func test_examinerWithInMemoryQuestionsLoader_deliversFullScoreWhenRespondingWithCorrectAnswers() throws {
         let inMemoryQuestionsLoader = InMemoryQuestionsLoader()
         let examiner = Examiner(questionsLoader: inMemoryQuestionsLoader)
