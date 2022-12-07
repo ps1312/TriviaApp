cbcd3959dbd8ed062efa3b4ddfc4ce53b2774d1a
Paulo Sérgio <ps131296@gmail.com>
2022-12-07 09:51:08 -0300

    add `TriviaEngine` framework

---
+++ TriviaEngine/TriviaEngine/TriviaEngine.h
@@ -0,0 +1,18 @@
+//
+//  TriviaEngine.h
+//  TriviaEngine
+//
+//  Created by Paulo Sergio da Silva Rodrigues on 07/12/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TriviaEngine.
+FOUNDATION_EXPORT double TriviaEngineVersionNumber;
+
+//! Project version string for TriviaEngine.
+FOUNDATION_EXPORT const unsigned char TriviaEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TriviaEngine/PublicHeader.h>
+
+
