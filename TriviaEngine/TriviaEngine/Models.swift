e33a725bf1a588240ea600d3fc4f1c45cac58339
Paulo Sérgio <ps131296@gmail.com>
2022-12-07 10:40:41 -0300

    `Examiner.prepare()` messages `QuestionsLoader` to load questions

--- TriviaEngine/TriviaEngine/Models.swift
+++ TriviaEngine/TriviaEngine/Models.swift
@@ -1,12 +1,23 @@
 import Foundation
 
-public struct Answer {
+public struct Answer: Equatable {
     public let id: UUID
     public let text: String
+
+    public init(id: UUID, text: String) {
+        self.id = id
+        self.text = text
+    }
 }
 
-public struct Question {
+public struct Question: Equatable {
     public let title: String
     public let answers: [Answer]
     public let correctAnswer: Answer
+
+    public init(title: String, answers: [Answer], correctAnswer: Answer) {
+        self.title = title
+        self.answers = answers
+        self.correctAnswer = correctAnswer
+    }
 }
--- TriviaEngine/TriviaEngineTests/ExaminerTests.swift
+++ TriviaEngine/TriviaEngineTests/ExaminerTests.swift
@@ -1,14 +1,46 @@
 import XCTest
 import TriviaEngine
 
+protocol QuestionsLoader {
+    func load() throws -> [Question]
+}
+
 class Examiner {
+    private let questionsLoader: QuestionsLoader
     let questions = [Question]()
+
+    init(questionsLoader: QuestionsLoader) {
+        self.questionsLoader = questionsLoader
+    }
+
+    func prepare() {
+        _ = try? questionsLoader.load()
+    }
 }
 
 class ExaminerTests: XCTestCase {
     func test_init_startsWithNoQuestions() {
-        let sut = Examiner()
+        let spy = QuestionsLoaderSpy()
+        let sut = Examiner(questionsLoader: spy)
 
         XCTAssertTrue(sut.questions.isEmpty)
+    }
+
+    func test_prepare_messagesQuestionsLoader() {
+        let spy = QuestionsLoaderSpy()
+        let sut = Examiner(questionsLoader: spy)
+
+        sut.prepare()
+
+        XCTAssertEqual(spy.loadCallCount, 1)
+    }
+
+    private class QuestionsLoaderSpy: QuestionsLoader {
+        var loadCallCount = 0
+
+        func load() throws -> [Question] {
+            loadCallCount += 1
+            return []
+        }
     }
 }
