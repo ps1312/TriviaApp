d82dbe61d31cc8cbb116627835a443622dfe4b10
Paulo Sérgio <ps131296@gmail.com>
2022-12-07 16:16:06 -0300

    add iOS project

---
+++ TriviaApp/TriviaApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TriviaApp
+//
+//  Created by Paulo Sergio da Silva Rodrigues on 07/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
