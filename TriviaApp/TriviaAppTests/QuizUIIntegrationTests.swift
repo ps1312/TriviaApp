1168dac88637a14169646814be988e310a4b7453
Paulo Sérgio <ps131296@gmail.com>
2022-12-08 09:44:00 -0300

    explicitly use `completeLoadWithError` helper to clarify test flow

--- TriviaApp/TriviaAppTests/QuizUIIntegrationTests.swift
+++ TriviaApp/TriviaAppTests/QuizUIIntegrationTests.swift
@@ -12,9 +12,11 @@ class QuizUIIntegrationTests: XCTestCase {
 
     func test_startFailure_displaysRetryButton() {
         let (sut, spy) = makeSUT()
-        sut.loadViewIfNeeded()
+        spy.completeLoadWithError()
 
+        sut.loadViewIfNeeded()
         XCTAssertTrue(sut.isShowingStartRetry, "Expected retry button after questions loading has failed on start")
+
         sut.simulateTapOnRetry()
         XCTAssertEqual(spy.startCallCount, 2, "Expected another start after user requests retry")
     }
@@ -32,10 +34,16 @@ class QuizUIIntegrationTests: XCTestCase {
 
     private class ExaminerSpy: ExaminerDelegate {
         var startCallCount = 0
+        var startResult: Question?
 
         func start() throws -> Question {
             startCallCount += 1
-            throw NSError(domain: "test error", code: 999)
+
+            if startResult == nil {
+                throw NSError(domain: "test error", code: 999)
+            }
+
+            return Question(id: UUID(), title: "", answers: [], correctAnswer: Answer(id: UUID(), text: ""))
         }
 
         func respond(_ question: Question, with answer: Answer) -> Question? {
@@ -44,6 +52,10 @@ class QuizUIIntegrationTests: XCTestCase {
 
         func evaluate() -> Score {
             Score(points: 0, responses: [])
+        }
+
+        func completeLoadWithError() {
+            startResult = nil
         }
     }
 }
