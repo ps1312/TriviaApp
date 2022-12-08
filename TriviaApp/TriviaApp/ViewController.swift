07daca8b27a542d4c98adbedde2770e9b123978d
Paulo Sérgio <ps131296@gmail.com>
2022-12-08 08:21:50 -0300

    change Main storyboard layout to match the question screen layout

--- TriviaApp/TriviaApp/ViewController.swift
+++ TriviaApp/TriviaApp/ViewController.swift
@@ -1,19 +1,32 @@
-//
-//  ViewController.swift
-//  TriviaApp
-//
-//  Created by Paulo Sergio da Silva Rodrigues on 07/12/22.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UITableViewController {
+    override func viewWillAppear(_ animated: Bool) {
+        setToolbarItems([
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
+            UIBarButtonItem(title: "Submit", style: .plain, target: self, action: nil),
+            UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        ], animated: animated)
     }
 
+    override func viewDidLoad() {
+        title = "1 of 5"
+    }
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "OptionCell", for: indexPath)
+
+        var config = cell.defaultContentConfiguration()
+        config.text = indexPath.row == 0 ? "True" : "False"
+
+        cell.contentConfiguration = config
+        cell.accessoryType = indexPath.row == 0 ? .checkmark : .none
+
+        return cell
+    }
 }
 
