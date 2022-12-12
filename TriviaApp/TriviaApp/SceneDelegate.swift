e5a051db8602a3c8540b012a76538a06a94bff10
Paulo Sérgio <ps131296@gmail.com>
2022-12-12 15:00:34 -0300

    take control of `SceneDelegate` by instantiating a `QuizViewController`

--- TriviaApp/TriviaApp/SceneDelegate.swift
+++ TriviaApp/TriviaApp/SceneDelegate.swift
@@ -5,7 +5,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+
+        window = UIWindow(windowScene: scene)
+        configureView()
+    }
+
+    func configureView() {
+        let bundle = Bundle(for: QuizViewController.self)
+        let storyboard = UIStoryboard(name: "Main", bundle: bundle)
+        let navController = storyboard.instantiateInitialViewController() as! UINavigationController
+
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
     }
 
 }
---
+++ TriviaApp/TriviaAppTests/SceneDelegateTests.swift
@@ -0,0 +1,16 @@
+import XCTest
+@testable import TriviaApp
+
+class SceneDelegateTests: XCTest {
+    func test_sceneWillConnectToSession_configuresWindow() {
+        let window = UIWindow()
+        let sut = SceneDelegate()
+        sut.window = window
+
+        sut.configureView()
+
+        let root = sut.window?.rootViewController as! UINavigationController
+        XCTAssertNotNil(root, "Expect root view controller attached to window to be an UINavigationController")
+        XCTAssertNotNil(root.topViewController as! QuizViewController, "Expected navigation top view controller to be a QuizViewController")
+    }
+}
