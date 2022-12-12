0680c83a4c18e97412a2b14fc254c5700502f055
Paulo Sérgio <ps131296@gmail.com>
2022-12-12 16:49:37 -0300

    grouped `Quiz UI` into a separate folder

--- TriviaApp/TriviaApp/SceneDelegate.swift
+++ TriviaApp/TriviaApp/SceneDelegate.swift
@@ -14,12 +14,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func configureView() {
         let bundle = Bundle(for: QuizViewController.self)
-        let storyboard = UIStoryboard(name: "Quiz", bundle: bundle)
-        let navController = storyboard.instantiateInitialViewController() as! UINavigationController
-        let quizViewController = navController.topViewController as! QuizViewController
-        quizViewController.examiner = Examiner(questionsLoader: InMemoryQuestionsLoader())
+//        let storyboard = UIStoryboard(name: "Quiz", bundle: bundle)
+//        let navController = storyboard.instantiateInitialViewController() as! UINavigationController
+//        let quizViewController = navController.topViewController as! QuizViewController
+//        quizViewController.examiner = Examiner(questionsLoader: InMemoryQuestionsLoader())
 
-        window?.rootViewController = navController
+        let storyboard = UIStoryboard(name: "Results", bundle: bundle)
+
+        window?.rootViewController = storyboard.instantiateInitialViewController()
         window?.makeKeyAndVisible()
     }
 
