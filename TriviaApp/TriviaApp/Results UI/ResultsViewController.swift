4c74a4312b4c4384796a012a5985bc4b7bb501d6
Paulo Sérgio <ps131296@gmail.com>
2022-12-13 09:20:02 -0300

    results UI displays total score

--- TriviaApp/TriviaApp/Results UI/ResultsViewController.swift
+++ TriviaApp/TriviaApp/Results UI/ResultsViewController.swift
@@ -1,11 +1,16 @@
 import UIKit
+import TriviaEngine
 
 final class ResultsViewController: UITableViewController {
+    var score: Score?
+
+    @IBOutlet private(set) public var totalScoreLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Results"
+        totalScoreLabel.text = "Your score: \(score?.points ?? 0)"
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
--- TriviaApp/TriviaAppTests/ResultsUIIntegrationTests.swift
+++ TriviaApp/TriviaAppTests/ResultsUIIntegrationTests.swift
@@ -1,4 +1,5 @@
 import XCTest
+import TriviaEngine
 @testable import TriviaApp
 
 class ResultsUIIntegrationTests: XCTestCase {
@@ -8,13 +9,27 @@ class ResultsUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.title, "Results")
     }
 
-    private func makeSUT() -> ResultsViewController {
+    func test_view_displaysTotalScore() {
+        let score = Score(points: 1, responses: [])
+        let sut = makeSUT(score: score)
+
+        XCTAssertEqual(sut.totalScore, "Your score: 1")
+    }
+
+    private func makeSUT(score: Score = Score(points: 0, responses: [])) -> ResultsViewController {
         let bundle = Bundle(for: ResultsViewController.self)
         let storyboard = UIStoryboard(name: "Results", bundle: bundle)
         let navController = storyboard.instantiateInitialViewController() as! UINavigationController
         let sut = navController.topViewController as! ResultsViewController
+        sut.score = score
         sut.loadViewIfNeeded()
 
         return sut
+    }
+}
+
+extension ResultsViewController {
+    var totalScore: String? {
+        return totalScoreLabel.text
     }
 }
