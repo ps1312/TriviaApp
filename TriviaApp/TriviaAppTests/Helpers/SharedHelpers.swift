d1b4c5605d3ad0a2b998fa9312b8c1c63d188f36
Paulo Sérgio <ps131296@gmail.com>
2022-12-13 14:14:32 -0300

    add snapshot config + add quiz loading questions error snapshot

--- TriviaApp/TriviaAppTests/Helpers/SharedHelpers.swift
+++ TriviaApp/TriviaAppTests/Helpers/SharedHelpers.swift
@@ -1,8 +1,10 @@
-//
-//  SharedHelpers.swift
-//  TriviaAppTests
-//
-//  Created by Paulo Sergio da Silva Rodrigues on 13/12/22.
-//
-
 import Foundation
+import TriviaEngine
+
+func makeQuestion(title: String = "any title", answers: [Answer]? = nil) -> (Question, [Answer]) {
+    let correctAnswer = Answer(id: UUID(), text: "Correct answer")
+    let wrongAnswer = Answer(id: UUID(), text: "Wrong answer")
+    let question = Question(id: UUID(), title: title, answers: answers ?? [correctAnswer, wrongAnswer], correctIndex: 0)
+
+    return (question, answers ?? [correctAnswer, wrongAnswer])
+}
--- TriviaApp/TriviaAppTests/QuizSnapshotTests.swift
+++ TriviaApp/TriviaAppTests/QuizSnapshotTests.swift
@@ -1,8 +1,26 @@
-//
-//  QuizSnapshotTests.swift
-//  TriviaAppTests
-//
-//  Created by Paulo Sergio da Silva Rodrigues on 13/12/22.
-//
+import XCTest
+@testable import TriviaApp
 
-import Foundation
+class QuizSnapshotTests: XCTestCase {
+    func test_loadQuestionsError() {
+        let	(sut, spy) = makeSUT()
+        spy.completeLoadWithError()
+
+        let snapshot = SnapshotWindow(configuration: .iPhone13(style: .light), root: sut).snapshot()
+        record(snapshot: snapshot, named: "QUESTIONS_LOAD_ERROR_light")
+    }
+
+    private func makeSUT(onFinish: @escaping () -> Void = {}) -> (UINavigationController, ExaminerSpy) {
+        let bundle = Bundle(for: QuizViewController.self)
+        let storyboard = UIStoryboard(name: "Quiz", bundle: bundle)
+        let sut = storyboard.instantiateInitialViewController() as! UINavigationController
+        let viewController = sut.topViewController as! QuizViewController
+        let spy = ExaminerSpy()
+        viewController.examiner = spy
+        viewController.onFinish = onFinish
+        viewController.loadViewIfNeeded()
+
+        return (sut, spy)
+    }
+}
+
