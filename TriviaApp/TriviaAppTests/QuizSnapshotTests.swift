1b363cd2c180c4a7378b633e0aa55c9f9e6b1bf2
Paulo Sérgio <ps131296@gmail.com>
2022-12-13 15:20:37 -0300

    add answer selected snapshot

--- TriviaApp/TriviaAppTests/QuizSnapshotTests.swift
+++ TriviaApp/TriviaAppTests/QuizSnapshotTests.swift
@@ -3,33 +3,43 @@ import XCTest
 
 class QuizSnapshotTests: XCTestCase {
     func test_loadQuestionsError() {
-        let	(sut, spy) = makeSUT()
+        let	(navController, _, spy) = makeSUT()
         spy.completeLoadWithError()
 
-        assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "QUESTIONS_LOAD_ERROR_light")
-        assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "QUESTIONS_LOAD_ERROR_dark")
+        assert(snapshot: navController.snapshot(.iPhone13(style: .light)), named: "QUESTIONS_LOAD_ERROR_light")
+        assert(snapshot: navController.snapshot(.iPhone13(style: .dark)), named: "QUESTIONS_LOAD_ERROR_dark")
     }
 
     func test_loadQuestionSuccess() {
         let (question, _) = makeQuestion(title: "Any\nmultiline\nquestion")
-        let (sut, spy) = makeSUT()
+        let (navController, _, spy) = makeSUT()
         spy.completeLoadWithSuccess(question: question)
 
-        assert(snapshot: sut.snapshot(.iPhone13(style: .light)), named: "QUESTIONS_LOAD_SUCCESS_dark")
-        assert(snapshot: sut.snapshot(.iPhone13(style: .dark)), named: "QUESTIONS_LOAD_SUCCESS_light")
+        assert(snapshot: navController.snapshot(.iPhone13(style: .light)), named: "QUESTIONS_LOAD_SUCCESS_dark")
+        assert(snapshot: navController.snapshot(.iPhone13(style: .dark)), named: "QUESTIONS_LOAD_SUCCESS_light")
     }
 
-    private func makeSUT(onFinish: @escaping () -> Void = {}) -> (UINavigationController, ExaminerSpy) {
+    func test_answerSelected() {
+        let (question, _) = makeQuestion(title: "Any\nmultiline\nquestion")
+        let (navController, viewController, spy) = makeSUT()
+        spy.completeLoadWithSuccess(question: question)
+        viewController.simulateOptionIsSelected(at: 0)
+
+        assert(snapshot: navController.snapshot(.iPhone13(style: .light)), named: "ANSWER_SELECTED_dark")
+        assert(snapshot: navController.snapshot(.iPhone13(style: .dark)), named: "ANSWER_SELECTED_light")
+    }
+
+    private func makeSUT(onFinish: @escaping () -> Void = {}) -> (UINavigationController, QuizViewController, ExaminerSpy) {
         let bundle = Bundle(for: QuizViewController.self)
         let storyboard = UIStoryboard(name: "Quiz", bundle: bundle)
-        let sut = storyboard.instantiateInitialViewController() as! UINavigationController
-        let viewController = sut.topViewController as! QuizViewController
+        let navController = storyboard.instantiateInitialViewController() as! UINavigationController
+        let viewController = navController.topViewController as! QuizViewController
         let spy = ExaminerSpy()
         viewController.examiner = spy
         viewController.onFinish = onFinish
-        sut.loadViewIfNeeded()
+        navController.loadViewIfNeeded()
 
-        return (sut, spy)
+        return (navController, viewController, spy)
     }
 }
 
