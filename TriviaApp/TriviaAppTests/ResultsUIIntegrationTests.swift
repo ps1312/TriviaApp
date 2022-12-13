6aa44f230ad44671eb6af3da2e14cb9e8f9699fa
Paulo Sérgio <ps131296@gmail.com>
2022-12-13 09:11:28 -0300

    created `makeSUT` helper in results UI tests

--- TriviaApp/TriviaAppTests/ResultsUIIntegrationTests.swift
+++ TriviaApp/TriviaAppTests/ResultsUIIntegrationTests.swift
@@ -3,12 +3,18 @@ import XCTest
 
 class ResultsUIIntegrationTests: XCTestCase {
     func test_view_displaysTitle() {
+        let sut = makeSUT()
+
+        XCTAssertEqual(sut.title, "Results")
+    }
+
+    private func makeSUT() -> ResultsViewController {
         let bundle = Bundle(for: ResultsViewController.self)
         let storyboard = UIStoryboard(name: "Results", bundle: bundle)
         let navController = storyboard.instantiateInitialViewController() as! UINavigationController
         let sut = navController.topViewController as! ResultsViewController
         sut.loadViewIfNeeded()
 
-        XCTAssertEqual(navController.title, "Results")
+        return sut
     }
 }
