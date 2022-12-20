c41495d04e88f3008cdc1d565585c8060e4b1d38
Paulo Sérgio <ps131296@gmail.com>
2022-12-20 13:56:27 -0300

    clean `AppDelegate`

--- TriviaApp/TriviaApp/AppDelegate.swift
+++ TriviaApp/TriviaApp/AppDelegate.swift
@@ -1,12 +1,4 @@
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    func application(_: UIApplication, didFinishLaunchingWithOptions _: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        return true
-    }
-
-    func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-}
+class AppDelegate: UIResponder, UIApplicationDelegate {}
